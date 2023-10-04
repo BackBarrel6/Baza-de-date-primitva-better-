1a707a707a0b18b786d5af04a455ed2fd810c27f
BackBarrel6 <rares.dobrisan@gmail.com>
2023-10-04 22:43:39 +0300

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#include <C:\Users\flori\source\repos\ConsoleApplication1\Source1.cpp>
+using namespace std;
+class user
+{private:
+	string username[1005], password[1005];
+public:
+	void createuser(string newname, string newpass, int i)
+	{
+		username[i] = newname;
+		password[i] = newpass;
+	}
+	bool passcheck(string passcheck, string usercheck, int i) const
+	{
+		return(password[i] == passcheck && username[i]==usercheck);
+	}
+	void change(string passch, int i, bool t)
+	{
+		if (t)
+			password[i] = passch;
+		else
+			username[i] = passch;
+	}
+	string un(int i)
+	{
+		return username[i];
+	}
+	string pas(int i)
+	{
+		return password[i];
+	}
+};
+int act, con, i;
+string newname, newpass, newpass2;
+user can[1005];
+int main()
+{
+	cout << "Actiune: \n 1.Log in \n 2.New User \n 3.Change username or password \n 4.List Users \n";
+	while (true)
+	{
+		cin >> act;
+		switch (act)
+		{
+		case 1:
+			///de construit functia de logare si actiune lmao
+		case 2:
+			cout << "enter usernname and password \n";
+			cin >> newname >> newpass;
+			cout << "confirm pass \n";
+			cin >> newpass2;
+			if(newpass==newpass2)
+			can[++con].createuser(newname, newpass, con);
+			break;
+		case 3:
+			bool t;
+			cout << "enter username and old password \n";
+			cin >> newname >> newpass2;
+			cout << "do you want to change name or password, 1 for password and 0 for username \n";
+			cin >> t;
+			for(i=1;i<=con;i++)
+				if(can[i].passcheck(newname,newpass,i))
+				{
+					cout << "enter new password \n";
+					cin >> newpass;
+					can[i].change(newpass,i,t);
+					break;
+				}
+			break;
+		case 4:
+			cout << "username" << "   " << "password \n";
+			for (i = 1; i <= con; i++)
+				cout << can[i].un(i) << "   " << can[i].pas(i);
+		default:
+			cout << "wrong input \n";
+		};
+	}
+}
