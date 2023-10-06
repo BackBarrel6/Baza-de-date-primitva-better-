9b63fbc03c47a9c7b7ed166e02a0f6edd1d9231a
BackBarrel6 <rares.dobrisan@gmail.com>
2023-10-06 22:55:07 +0300

    rescriere cod principal

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,73 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class user
 {
 private:
-	string username[1005], password[1005];
+    string username, password;
+
 public:
-	void createuser(string newname, string newpass, int i)
+    void createuser(string newname, string newpass)
+    {
+        username = newname;
+        password = newpass;
+    }
+	/*int userreturn(string currentname, string currentpassword)
 	{
-		username[i] = newname;
-		password[i] = newpass;
-	}
-	bool passcheck(string passcheck, string usercheck, int i) const
-	{
-		return(password[i] == passcheck && username[i] == usercheck);
-	}
-	void change(string passch, int i, bool t)
-	{
-		if (t)
-			password[i] = passch;
-		else
-			username[i] = passch;
-	}
-	void lista(int i)
-	{
-		cout << username[i] << "   " << password[i] << " ";
-	}
+
+	}*/
 };
-int act, con, i;
+int act, con;
 string newname, newpass, newpass2;
-user can[1005];
+user n[1005];
 int main()
 {
-	cout << "Actiune: \n 1.Log in \n 2.New User \n 3.Change username or password \n 4.List Users \n";
-	while (true)
-	{
-		cin >> act;
-		switch (act)
-		{
-		case 1:
-			///de construit functia de logare si actiuni
-		case 2:
-			cout << "enter usernname and password \n";
-			cin >> newname >> newpass;
-			cout << "confirm pass \n";
-			cin >> newpass2;
-			if (newpass == newpass2)
-				can[++con].createuser(newname, newpass, con);
-			break;
-		case 3:
-			bool t;
-			cout << "enter username and old password \n";
-			cin >> newname >> newpass2;
-			cout << "do you want to change name or password, 1 for password and 0 for username \n";
-			cin >> t;
-			for (i = 1; i <= con; i++)
-				if (can[i].passcheck(newname, newpass, i))
-				{
-					cout << "enter new password \n";
-					cin >> newpass;
-					can[i].change(newpass, i, t);
-					break;
+    cout << "enter action \n 1.Log in \n 2.Create user \n 3.Change password or username \n 4.List users \n 5.(admin)Read users from file \n"; 
+    cin >> act;
+    while (true)
+    {
+        switch (act)
+        {
+        case 1:
+            /// de scris asta ca mie lene
+        case 2:
+            cout << "enter username and password";
+            cin >> newname >> newpass;
+            cout << "enter password again";
+            cin >> newpass2;
+            if (newpass == newpass2)
+                {
+					n[++con].createuser(newname, newpass);
+					cout<<"user succesfully created, enter new action";
 				}
-			break;
-		case 4:
-			cout << "username" << "   " << "password \n";
-			for (i = 1; i <= con; i++)
-				can[i].lista(i);
-		default:
-			cout << "wrong input \n";
-		};
-	}
+            break;
+		case 3:
+		int actcase3;
+			cout<<"enter old username and password \n";
+		cin>>newname>>newpass;
+			cout<< "what do you want to change: 1 for username \n 2 for password \n";
+				cin>>actcase3;
+			switch(actcase3)
+			{case 1:
+
+
+			};
+        }
+    };
 }
